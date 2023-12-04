b159f70e8093a7282f8391839b1fd5a4101cdfcb
ethan-wickstrom <e.t.wickstrom@wustl.edu>
2023-12-04 02:05:40 -0600

    Introduced Card component and streamlined Welcome page
    Refactored the Welcome page to utilize a new Card component for better modularity and reduced code duplication. Removed verbose and repetitive markup, replacing it with instances of the newly created Card component, which encapsulates the common card design and behavior patterns. This change enhances code maintainability and readability. Additionally, set up FontAwesome for scalable icons, improving UI flexibility and reducing reliance on SVG elements directly embedded in the templates. The refactoring supports future scalability and ease of updates to the Welcome page content and design.

--- api/resources/js/app.ts
+++ api/resources/js/app.ts
@@ -7,6 +7,26 @@ import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import { faArrowRight, faCamera, faNewspaper, faShareNodes, faTerminal } from '@fortawesome/free-solid-svg-icons'
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+
+/* add icons to the library */
+library.add(
+  faCamera,
+  faTerminal,
+  faNewspaper,
+  faShareNodes,
+  faArrowRight,
+  faGithub
+)
+
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
     resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob<DefineComponent>('./Pages/**/*.vue')),
@@ -14,6 +34,8 @@ createInertiaApp({
         createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(ZiggyVue, Ziggy)
+            .component('font-awesome-icon', FontAwesomeIcon)
+            .mixin({ methods: { route: window.route } })
             .mount(el);
     },
     progress: {
